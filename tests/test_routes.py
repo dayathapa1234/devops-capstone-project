3a585a57728338045e5c31663ef4534b53611225
Daya Thapa <dayathapa@gmail.com>
2024-04-18 06:54:10 -0400

    Adding Delete Feature

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -174,3 +174,11 @@ class TestAccountService(TestCase):
         resp = self.client.put(f"{BASE_URL}/0", json={})
         # assert that the resp.status_code is status.HTTP_404_NOT_FOUND
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        # send a self.client.delete() request to the BASE_URL with an id of an account
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        # assert that the resp.status_code is status.HTTP_204_NO_CONTENT
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
