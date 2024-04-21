638daf9b58bd56ccddf2f2d1e00231e75f6cea57
Daya Thapa <dayathapa@gmail.com>
2024-04-21 16:44:45 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -19,7 +19,8 @@ DATABASE_URI = os.getenv(
 )
 
 BASE_URL = "/accounts"
-HTTPS_ENVIRON = {'wsgi.url_scheme':'https'}
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
+
 
 ######################################################################
 #  T E S T   C A S E S
@@ -152,10 +153,9 @@ class TestAccountService(TestCase):
         # create an Account to update
         test_account = AccountFactory()
         # send a self.client.post() request to the BASE_URL with a json payload of test_account.serialize()
-        resp = self.client.post(BASE_URL,json=test_account.serialize())
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
         # assert that the resp.status_code is status.HTTP_201_CREATED
         self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
-        
         # update the account
         # get the data from resp.get_json() as new_account
         new_account = resp.get_json()
@@ -195,7 +195,7 @@ class TestAccountService(TestCase):
         # get the data from resp.get_json()
         data = resp.get_json()
         # assert that the len() of the data is 5 (the number of accounts you created)
-        self.assertEqual(len(data),5)
+        self.assertEqual(len(data), 5)
 
     def test_method_not_allowed(self):
         """It should not allow an illegal method call"""
@@ -221,3 +221,4 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check fot the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+        
